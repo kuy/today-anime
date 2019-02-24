1f7d9af40fffb982e8751763ed294895ea7fd3d5
Yuki Kodama <endflow.net@gmail.com>
2019-02-24 13:05:19 +0900

    Open CalendarLib

--- app/program.ml
+++ app/program.ml
@@ -1,8 +1,9 @@
 open Core
+open CalendarLib
 
 type t = Syobocal.program
 
 let to_string (program:t) =
   let count = Option.value ~default:"?" program.count in
   let subtitle = Option.value ~default:"[Untitled]" program.subtitle in
-  sprintf "%s: %s [%s] %s" (CalendarLib.Printer.Date.sprint "%Y/%m/%d" (CalendarLib.Date.from_unixfloat @@ Float.of_string program.start_time)) program.title count subtitle
+  sprintf "%s: %s [%s] %s" (Printer.Date.sprint "%Y/%m/%d" (Date.from_unixfloat @@ Float.of_string program.start_time)) program.title count subtitle
