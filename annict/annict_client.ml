f437169d6b9f466bf200398aebceefd5337d157c
Yuki Kodama <endflow.net@gmail.com>
2019-03-10 13:01:02 +0900

    Put tls first

--- annict/annict_client.ml
+++ annict/annict_client.ml
@@ -5,5 +5,6 @@ let base_url = "https://api.annict.com"
 let make_url path = Uri.of_string (base_url ^ path)
 
 let get token url =
+  let config = Conduit_async.V2.Ssl.Config.create ~hostname: "api.annict.com" () in
   let headers = Cohttp.Header.init_with "Authorization" ("Bearer " ^ token) in
-  Cohttp_async.Client.get ~headers: headers url
+  Cohttp_async.Client.get ~ssl_config: config ~headers: headers url
